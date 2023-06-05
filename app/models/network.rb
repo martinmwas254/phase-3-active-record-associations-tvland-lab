64393f0f09fe9999e4162638b2f7f35be3566fab
Martin <Martin@Martin>
2023-06-05 14:21:10 +0300

    completed assignment

--- app/models/network.rb
+++ app/models/network.rb
@@ -1,3 +1,12 @@
 class Network < ActiveRecord::Base
+    has_many :shows
   
-end
+    def shows
+      Show.where(network_id: id)
+    end
+  
+    def sorry
+      "We're sorry about passing on John Mulaney's pilot"
+    end
+  end
+  
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,20 @@
 class Show < ActiveRecord::Base
+    has_many :characters
+    has_many :actors, through: :characters
+    belongs_to :network
   
-end
+    def character_list
+      characters.all
+    end
+  
+    def network_name
+      network.name
+    end
+  
+    def actors_list
+        self.actors.collect do |char|
+            "#{char.first_name} #{char.last_name}"
+        end.join
+    end
+  end
+  
